4dde87b323d28edd635ab0644d5c8a927e375079
Santeri Mertakorpi <santemer@uef.fi>
2021-10-17 18:02:34 +0300

    build

--- osa3/puhelinluettelo/src/services/persons.js
+++ osa3/puhelinluettelo/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = '/api/persons'
+const baseUrl = 'https://murmuring-castle-54260.herokuapp.com/api/persons'
 
 const getAll = () =>{
     const request = axios.get(baseUrl)
