3f1c92ac6e9e20c91ee0561c010ca1ea8deb6d96
Santeri Mertakorpi <santemer@uef.fi>
2021-10-17 17:13:51 +0300

    puhelinluettelo-osa3

---
+++ osa3/puhelinluettelo/src/App.js
@@ -0,0 +1,121 @@
+
+import React, { useState, useEffect } from 'react'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persosns'
+import personService from './services/persons'
+import Notification from './components/Notification'
+
+const App = () => {
+  const [persons, setPersons] = useState([]) 
+  const [newName, setNewName ] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [newFilter, setNewFilter] = useState('')
+  const [message, setMessage] = useState(null)
+  
+
+  useEffect(() =>{
+    personService
+      .getAll()
+      .then(initialPersons =>{
+        setPersons(initialPersons)
+      })
+  },[])
+
+  
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const nameObject = {
+        name: newName,
+        number: newNumber
+      }
+    
+    let mapped = persons.map(p => p.name)
+    
+    
+    if(mapped.includes(newName)){
+      window.alert(`${newName} is already added to phonebook`)
+      setNewName('')
+      setNewNumber('')
+    }else{
+      personService
+        .create(nameObject)
+        .then(returnedPerson =>{
+          setPersons(persons.concat(returnedPerson))
+          setMessage(`Added ${newName} `)
+          setNewName('')
+          setNewNumber('')
+          setTimeout(()=>{
+            setMessage(null)
+          },5000)
+        })
+      
+      
+    }
+  }
+  const deletePerson =id=>{
+    const person = persons.find(p=>p.id===id)
+    if(window.confirm(`Delete ${person.name} ?`)){
+      personService
+        .remove(id)
+        .then(returnedPersons =>{
+          setPersons(persons.filter(p=>p.id!==id))
+          setMessage(`Deleted ${person.name}`)
+          setTimeout(()=>{
+            setMessage(null)
+          },5000)
+        })
+    }
+  }
+  const handleNameChange = (event) =>{
+    setNewName(event.target.value)
+  }
+  const handelNumberChange = (event) =>{
+    setNewNumber(event.target.value)
+  }
+  
+  const handleFilterChange = (event) =>{
+    setNewFilter(event.target.value)
+    if(showAll)
+      setShowAll(showAll===false)
+  }
+  const personsToShow = showAll
+    ? persons
+    : persons.filter(person=> person.name.toLowerCase().includes(newFilter))
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Notification message={message} />
+      <Filter 
+        newFilter={newFilter} 
+        handleFilterChange={handleFilterChange} 
+      />
+      <h3>add a new </h3>
+      
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handelNumberChange={handelNumberChange} 
+      />
+
+      <h3>Numbers</h3>
+
+      {personsToShow.map(person =>
+        <Persons key={person.name} person={person} deletePerson={()=>deletePerson(person.id)}/>
+      )}
+      
+      
+    </div>
+  )
+
+}
+
+
+
+
+export default App
